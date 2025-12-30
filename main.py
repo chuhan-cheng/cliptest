8279962b61ed526fda202047c5e8b74465049192
student <student@localhost.localdomain>
2025-12-31 02:27:02 +0800

    update

--- main.py
+++ main.py
@@ -10,23 +10,30 @@ class LazyMimeData(QMimeData):
     def __init__(self):
         super().__init__()
         # 宣告我們可以提供純文字格式
-        self.setData("text/plain", b"yoooooo") 
+        self.setData("text/plain", b"") 
+        self.activated = False
+        # self.setData("x-kde-passwordManagerHint", b"secret")
+        # self.setData("application/x-copyq-ignore", b"")
 
     def retrieveData(self, mime_type, preferred_type):
         """
         這是核心 Callback！
         當其他程式按下「貼上」時，作業系統會來問我們要資料。
         """
+        if self.activated:
+            print("內容已經產生過一次，直接回傳快取的資料。")
+            return super().retrieveData(mime_type, preferred_type)
         print(f"retrieveData 被呼叫，mime_type: {mime_type}, preferred_type: {preferred_type}")
         if mime_type == "text/plain":
             print("偵測到貼上動作！正在即時生成內容...")
             sleep(1)  # 模擬耗時操作
-            
+            self.activated = True
             # 在這裡執行你的邏輯，例如讀取資料庫或處理字串
             # current time
             currentTime = time()
             generated_text = f"這是由 A 程式即時產生的內容 (Lazy Copy) time: {currentTime}"
-            print(f"內容生成完成，傳回給系統剪貼簿。 generated_text: {generated_text}")
+            self.setData("text/plain", generated_text.encode('utf-8')) 
+            # print(f"內容生成完成，傳回給系統剪貼簿。 generated_text: {generated_text}")
             return QVariant(generated_text)
         
         return super().retrieveData(mime_type, preferred_type)
@@ -51,6 +58,7 @@ class Backend(QObject):
         # 建立 Lazy 物件
         lazy_data = LazyMimeData()
         # 注意：在某些平台上，這需要保持程式 A 運行，否則剪貼簿會遺失
+        print("AAAA")
         self.clipboard.setMimeData(lazy_data)
 
 def main():
